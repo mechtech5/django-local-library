fb98ad524bb595fbd3b657077e50f6b7126a3a7c
Ayush Likhar <alikhar@laxyo.in>
2019-08-28 11:39:05 +0530

    added models

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from django.views.generic import RedirectView
-from . import view
+from . import views
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
+    
 ]
